0a4aabea39fff53c4fa535c6747f083effeb528c
Mik0ru <aziztermecikov5@gmail.com>
2025-02-01 17:55:43 +0600

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 android {
