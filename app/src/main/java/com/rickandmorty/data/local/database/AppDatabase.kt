5656e875ceada070266a57e4df7eff0fb55c8849
Mik0ru <aziztermecikov5@gmail.com>
2025-02-02 19:00:13 +0600

    commit

--- app/src/main/java/com/rickandmorty/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/rickandmorty/data/local/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.rickandmorty.data.local.database.dao.FavoritesDao
 import com.rickandmorty.data.local.models.CharacterEntity
 
-@Database(entities = [CharacterEntity::class], version = 3)
+@Database(entities = [CharacterEntity::class], version = 4)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun favoritesDao(): FavoritesDao
 
